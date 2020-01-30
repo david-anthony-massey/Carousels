89ab38b0a6f11a35c88f894bd4513eff126eb29c
Mark Wallace <mwallace999@gmail.com>
2020-01-30 11:14:12 -0600

    Fixed rating issues

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Rating({product, goToRating}) {
   let rating = [];
   let image = '';
-  for (let i = 0; i < 5; i++) {
+  for (let i = 1; i < 6; i++) {
     if (i <= product.rating) {
       image = 'styles/images/full_leaf.png';
-    } else if (i - product.rating < .25) {
+    } else if (i - product.rating <= .25) {
       image = 'styles/images/full_leaf.png';
     } else if (i - product.rating > .25 && i - product.rating < .75) {
       image = 'styles/images/half_leaf.png';
