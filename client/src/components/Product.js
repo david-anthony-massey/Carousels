96d3426524c4165a653a737df5928612888db7e6
Mark Wallace <mwallace999@gmail.com>
2020-01-30 02:01:03 -0600

    First custom Ratings component

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import Rating from './Rating.js';
 import StarRatingComponent from 'react-star-rating-component';
 
 export default function Product(props) {
-  console.log(props);
   return (
     <div className='productContainer'>
       {Array.from(props.productsFive).map((product, index) => {
         return(
           <div key={index} className='productBlock'>
-            <img className='image' src={product.imgUrl} id={product.productId} data-cat={product.categoryId} onClick={props.goToProduct}></img>
+            <img className='image' src={product.imgUrl} id={product.productId} data-cat={product.categoryId} onClick={props.goToProduct} />
             <div className='textBox'>
               <div className='nameText' id={product.productId} onClick={props.goToProduct}>{product.productName}</div>
-              <StarRatingComponent
-                  name={'rating'+index}
-                  editing={false}
-                  renderStarIcon={() => <span id={product.productId} data-cat={product.categoryId} onClick={props.goToRating} className='rating'><img src="styles/images/full_leaf.png" alt='test' height='27px' width='27px' /></span>}
-                  renderStarIconHalf={() => <span id={product.productId} data-cat={product.categoryId} onClick={props.goToRating} className='rating'><img src="styles/images/half_leaf_bordered.png" alt='test' height='25px' width='25px' margin='5px'/></span>}
-                  starCount={Math.round(product.rating)}
-              />
+              <Rating product={product} goToRating={props.goToRating} />
               <div className='priceText' id={product.productId} onClick={props.goToProduct}>${(product.price).toFixed(2)}</div>
             </div>
           </div>
---
+++ client/src/components/Rating.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function Rating({product, goToRating}) {
+  let rating = [];
+  let image = '';
+  for (let i = 0; i < 5; i++) {
+    if (i <= product.rating) {
+      image = 'styles/images/full_leaf.png';
+    } else if (i - product.rating < .25) {
+      image = 'styles/images/full_leaf.png';
+    } else if (i - product.rating > .25 && i - product.rating < .75) {
+      image = 'styles/images/half_leaf.png';
+    } else {
+      image = 'styles/images/empty_leaf.png';
+    }
+
+    rating.push(
+      <img 
+        key={i}
+        src={image}
+        alt='test' id={product.productId} 
+        data-cat={product.categoryId}  
+        className='rating' 
+        onClick={goToRating} 
+        height='27px' 
+        width='27px' 
+      />
+    )
+  }
+
+  return (
+    <div>
+      {rating}
+    </div>
+  )
+}
