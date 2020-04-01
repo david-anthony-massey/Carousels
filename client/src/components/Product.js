a89862f66ed4373e6bf646af5923680e982c242e
David M <d.anthony.massey@gmail.com>
2020-04-01 08:13:13 -0500

    reconfigure for Heroku

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -9,14 +9,24 @@ export default function Product(props) {
           <div key={index} className="carousel-productBlock">
             <img
               className="carousel-image"
-              src={product.imgUrl}
+              src={product.imgURL}
               id={product.productId}
               // onClick={props.goToProduct}
             />
             <div className="carousel-textBox">
-              <div className="carousel-nameText" id={product.productId}/*onClick={props.goToProduct}*/>{product.productName}</div>
+              <div
+                className="carousel-nameText"
+                id={product.productId} /*onClick={props.goToProduct}*/
+              >
+                {product.productName}
+              </div>
               <Rating product={product} /*goToRating={props.goToRating}*/ />
-              <div className="carousel-priceText" id={product.productId}/*onClick={props.goToProduct}*/>${(product.price).toFixed(2)}</div>
+              <div
+                className="carousel-priceText"
+                id={product.productId} /*onClick={props.goToProduct}*/
+              >
+                ${product.price.toFixed(2)}
+              </div>
             </div>
           </div>
         );
