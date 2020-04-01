dbb605b269f11370b1c578ccbc2669bde884ca49
David M <d.anthony.massey@gmail.com>
2020-04-01 05:07:49 -0500

    reconfigure for Heroku

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import StarRatingComponent from 'react-star-rating-component';
+import React from "react";
+import StarRatingComponent from "react-star-rating-component";
 
 export default function Product(props) {
   return (
-    <div className='productContainer'>
+    <div className="productContainer">
       {Array.from(props.productsFive).map((product, index) => {
-        return(
-          <div key={index} className='productBlock'>
-            <img className='image' src={product.imgUrl} id={product.productId} data-cat={product.categoryId} onClick={props.goToProduct}></img>
-            <div className='textBox'>
-              <div className='nameText' id={product.productId} onClick={props.goToProduct}>{product.productName}</div>
-                <div>
-                  <StarRatingComponent
-                    name={product.productId.toString()}
-                    editing={false}
-                    renderStarIcon={() => <span id={product.productId} data-cat={product.categoryId} onClick={props.goToRating} className='rating'>🍁</span>}
-                    starCount={Math.round(product.rating)}
-                  />
-                </div>
-              <div className='priceText' id={product.productId} onClick={props.goToProduct}>${(product.price).toFixed(2)}</div>
+        return (
+          <div key={index} className="productBlock">
+            <img
+              className="image"
+              src={product.imgUrl}
+              id={product.id}
+              data-cat={product.category_id}
+              onClick={props.goToProduct}
+            ></img>
+            <div className="textBox">
+              <div
+                className="nameText"
+                id={product.id}
+                onClick={props.goToProduct}
+              >
+                {product.name}
+              </div>
+              <div>
+                <StarRatingComponent
+                  name={product.productId.toString()}
+                  editing={false}
+                  renderStarIcon={() => (
+                    <span
+                      id={product.id}
+                      data-cat={product.category_id}
+                      onClick={props.goToRating}
+                      className="rating"
+                    >
+                      🍁
+                    </span>
+                  )}
+                  starCount={Math.round(product.rating)}
+                />
+              </div>
+              <div
+                className="priceText"
+                id={product.id}
+                onClick={props.goToProduct}
+              >
+                ${product.price.toFixed(2)}
+              </div>
             </div>
           </div>
-        )
+        );
       })}
     </div>
   );
